41e1cdecb47073cd94eff77c6f80c23791f22bc2
dydeepak97 <dydeepak97@example.com>
2017-08-27 02:18:33 +0530

    Lec 14

--- work/week2/Lecture14/beginning-of-lecture-code/app.js
+++ work/week2/Lecture14/beginning-of-lecture-code/app.js
@@ -6,10 +6,34 @@ angular.module('CounterApp', [])
 
 CounterController.$inject = ['$scope'];
 function CounterController($scope) {
-
+  $scope.onceCounter = 0;
+  $scope.counter= 0;
+  $scope.name= "DY";
   $scope.showNumberOfWatchers = function () {
-
+    console.log('# of watchers : ', $scope.$$watchersCount);
   };
+
+  $scope.countOnce = function(){
+    $scope.onceCounter = 1;
+  };
+
+  $scope.upCounter = function(){
+    $scope.counter++;
+  };
+
+  $scope.$watch(function (){
+    console.log("Digest loop fired!");
+  });
+
+//  $scope.$watch('onceCounter' , function(newValue , oldvalue){
+//    console.log('oldvalue: ',oldvalue);
+//    console.log('newvalue: ',newValue);
+//  });
+//
+//  $scope.$watch('counter' , function(newValue , oldvalue){
+//    console.log('couner oldvalue: ',oldvalue);
+//    console.log('counter newvalue: ',newValue);
+//  });
 
 }
 
